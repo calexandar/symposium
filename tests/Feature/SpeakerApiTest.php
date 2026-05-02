5ca1fc03181d05459f061378934609391748c1fe
calexandar <alexandercvet243@gmail.com>
2026-05-02 20:50:38 +0200

    feat: speakers api test

--- tests/Feature/SpeakerApiTest.php
+++ tests/Feature/SpeakerApiTest.php
@@ -1,7 +1,16 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use Illuminate\Testing\Fluent\AssertableJson;
 
-    $response->assertStatus(200);
+test('it gets speakers from the api', function () {
+   $response = $this->get('/api/speakers');
+
+    $response
+        ->assertJson(fn (AssertableJson $json) =>
+            $json->has(3)
+                ->first(fn ($json) =>
+                    $json->where('name', 'John Doe')
+                        ->where('email', 'lM4kT@example.com')
+                )
+        );
 });
