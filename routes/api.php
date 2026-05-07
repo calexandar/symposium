2626d1f03c8962bdc0e0d4580a3fd0e3e9858f83
calexandar <alexandercvet243@gmail.com>
2026-05-07 09:45:14 +0200

    ade api routes

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('throttle:public_api')->group(function (): void {
-    Route::get('conferences', fn() => 'conferences');
-    Route::get('speakers', fn() => SpeakerResource::collection(User::all()));
+    Route::get('/conferences', fn() => 'conferences');
+    Route::get('/speakers', fn() => SpeakerResource::collection(User::all()));
     Route::get('/user', fn(Request $request) => $request->user())->middleware('auth:sanctum');
 });
